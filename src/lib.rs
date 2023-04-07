d0a46ecefba062c4f9c8924e195d0e0c37ef1070
enn3 <arturpeshko39@gmail.com>
2023-04-07 16:17:53 +0200

    Added the skeleton of the top panel

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
 mod app;
+mod widgets;
+
 pub use app::TemplateApp;
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ fn main() -> eframe::Result<()> {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "eframe template",
+        "GAS",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(gas::TemplateApp::new(cc))),
     )
 }
 
